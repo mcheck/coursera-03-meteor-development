6faf6b485d55c4f9cfe9e1e6c453ab3dbb525757
Michael Check <mcheck@sologroup.com>
2016-01-26 10:12:11 -0600

    Update from week 2, lesson 4

--- week-02/image_share/image_share.js
+++ week-02/image_share/image_share.js
@@ -3,30 +3,23 @@ Images = new Mongo.Collection("images");
 console.log(Images.find().count());
 
 if (Meteor.isClient) {
-   var img_data = [
-   {
-      img_src:"laptops.jpg",
-      img_alt:"some laptops on a table" 
-   }, 
-   {
-      img_src:"bass.jpg",
-      img_alt:"a bass player" 
-   }, 
-   {
-      img_src:"beard.jpg",
-      img_alt:"some musicians with beards" 
-   }, 
 
-   ];
-
-   Template.images.helpers({images:img_data});
-
+   Template.images.helpers({images:Images.find()});
 
    Template.images.events({
-    'click .js-image':function(event){
-        $(event.target).css("width", "50px");
-    }
-   });
+             'click .js-image': function(event) {
+                 $(event.target).css("width", "50px");
+             },
+             'click.js-del-image': function(event) {
+                 var image_id = this._id;
+                 $("#"+image_id).hide('slow', function(){
+                   Images.remove({"_id":image_id}); 
+                 })
+                 
+                 console.log("You have deleted: "+image_id);
+             }
+
+   }); // end template image events
 
 }
 
