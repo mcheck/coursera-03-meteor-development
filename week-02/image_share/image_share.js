ea12bf00a1144c5271696f0ea68c9b60c693e45a
Michael Check <mcheck@sologroup.com>
2016-01-26 11:15:25 -0600

    complete week 2 lesson 5

--- week-02/image_share/image_share.js
+++ week-02/image_share/image_share.js
@@ -4,29 +4,38 @@ console.log(Images.find().count());
 
 if (Meteor.isClient) {
 
-   Template.images.helpers({images:Images.find()});
+    Template.images.helpers({
+        images: Images.find({}, {sort: {rating: -1}} )
+    });
 
-   Template.images.events({
-             'click .js-image': function(event) {
-                 $(event.target).css("width", "50px");
-             },
-             'click.js-del-image': function(event) {
-                 var image_id = this._id;
-                 $("#"+image_id).hide('slow', function(){
-                   Images.remove({"_id":image_id}); 
-                 })
-                 
-                 console.log("You have deleted: "+image_id);
-             }
+    Template.images.events({
+        'click .js-image': function(event) {
+            $(event.target).css("width", "50px");
+        },
+        'click .js-del-image': function(event) {
+            var image_id = this._id;
+            $("#" + image_id).hide('slow', function() {
+                Images.remove({
+                    "_id": image_id
+                });
+            })
+        },
+        'click .js-rate-image': function(event) {
+            var rating = $(event.currentTarget).data("userrating");
+            var image_id = this.image_id;
+            Images.update({_id:image_id},
+                          {$set: {rating:rating}});
+            console.log("userrating: "+rating+" for image id: "+image_id);
 
-   }); // end template image events
+        } // end js-rate-imate
+
+    }); // end template image events
 
 }
 
 if (Meteor.isServer) {
-   console.log("I am the server");
+    console.log("I am the server");
 }
 
 
 console.log("where am I running");
-
