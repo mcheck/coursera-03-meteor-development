92d8706277933ce05904bddab525b96fc388481a
Michael Check <mcheck@sologroup.com>
2016-01-26 15:56:08 -0600

    Completed week 3, lesson 8

--- week-03/image_share/image_share.js
+++ week-03/image_share/image_share.js
@@ -7,7 +7,14 @@ if (Meteor.isClient) {
   });
 
     Template.images.helpers({
-        images: Images.find({}, {sort: {createdOn: -1, rating: -1}} ),
+        images:function(){
+          if (Session.get("userFilter")){   // a filter has been set!
+            return Images.find({createdBy: Session.get("userFilter")}, {sort: {createdOn: -1, rating: -1}} );
+          } else {
+            return Images.find({}, {sort: {createdOn: -1, rating: -1}} );
+          }
+        },
+
         getUser: function(user_ID){
           var user = Meteor.users.findOne({_id:user_ID});
           if (user) {
@@ -15,25 +22,50 @@ if (Meteor.isClient) {
           } else {
             return "anon";
           }
+        },
+
+        filtering_images:function(){
+          if (Session.get("userFilter")){   // a filter has been set!
+            return true;
+          } else {
+            return false;
+          }
+        },
+        
+        getFilterUser: function() {
+            if (Session.get("userFilter")) { // a filter has been set!
+                var user = Meteor.users.findOne({
+                    _id: Session.get("userFilter")
+                });
+                if (user) {
+                    return user.username;
+                } else {
+                    return "anon";
+                }
+            }
         }
+
+        });
+
+    Template.body.helpers({
+        username: function() {
+            console.log(Meteor.user());
+            if (Meteor.user()) {
+                return Meteor.user().username;
+            } else {
+                return "Anonymous Internetsess User";
+            }
+        }
+
     });
 
-    Template.body.helpers({username:function(){
-       console.log(Meteor.user());
-      if (Meteor.user()) {
-        return Meteor.user().username;
-      } else {
-        return "Anonymous Internetsess User"; 
-      }
-    }
-        
-    });
 
 
     Template.images.events({
         'click .js-image': function(event) {
             $(event.target).css("width", "50px");
         },
+
         'click .js-del-image': function(event) {
             var image_id = this._id;
             $("#" + image_id).hide('slow', function() {
@@ -41,18 +73,29 @@ if (Meteor.isClient) {
                     "_id": image_id
                 });
             })
-        },
+        }, // end js-del-image
+
         'click .js-rate-image': function(event) {
             var rating = $(event.currentTarget).data("userrating");
             var image_id = this.image_id;
             Images.update({_id:image_id},
                           {$set: {rating:rating}});
             console.log("userrating: "+rating+" for image id: "+image_id);
-
         }, // end js-rate-imate
+
         'click .js-show-image-form': function(event) {
             $("#image_add_form").modal("show");
-        } //end 
+        }, //end js-show-image-form
+
+        'click .js-set-image-filter': function(event) {
+            Session.set("userFilter", this.createdBy);
+            Session.set("", value);
+        }, // end js-set-image-filter
+
+        'click .js-rm-image-filter': function(event) {
+            Session.set("userFilter", undefined);
+        } // end js-rm-image-filter
+
 
     }); // end template image events
 
