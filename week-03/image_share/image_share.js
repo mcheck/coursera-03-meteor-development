b28e162a8f7e7c0e9821430776366b75428cb608
Michael Check <mcheck@sologroup.com>
2016-01-26 16:28:28 -0600

    Completed week 3

--- week-03/image_share/image_share.js
+++ week-03/image_share/image_share.js
@@ -2,6 +2,27 @@ Images = new Mongo.Collection("images");
 
 if (Meteor.isClient) {
 
+  Session.set("imageLimit", 8);
+
+  lastScrollTop = 0; 
+  $(window).scroll(function(event){
+    // test if we are near the bottom of the window
+    if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+      // where are we in the page? 
+      var scrollTop = $(this).scrollTop();
+      // test if we are going down
+      if (scrollTop > lastScrollTop){
+        // yes we are heading down...
+       Session.set("imageLimit", Session.get("imageLimit") + 4);
+      }
+
+      lastScrollTop = scrollTop;
+    }
+        
+  })
+
+
+  // user authentication, use username and email
   Accounts.ui.config ({
     passwordSignupFields: "USERNAME_AND_EMAIL"
   });
@@ -9,14 +30,15 @@ if (Meteor.isClient) {
     Template.images.helpers({
         images:function(){
           if (Session.get("userFilter")){   // a filter has been set!
-            return Images.find({createdBy: Session.get("userFilter")}, {sort: {createdOn: -1, rating: -1}} );
+            return Images.find({createdBy: Session.get("userFilter")}, {sort: {createdOn: -1, rating: -1}, limit: Session.get("imageLimit")});
           } else {
-            return Images.find({}, {sort: {createdOn: -1, rating: -1}} );
+            return Images.find({}, {sort: {createdOn: -1, rating: -1}, limit: Session.get("imageLimit")});
           }
         },
 
         getUser: function(user_ID){
-          var user = Meteor.users.findOne({_id:user_ID});
+          var filter = {_id:user_ID};
+          var user = Meteor.users.findOne(filter)
           if (user) {
             return user.username;
           } else {
@@ -89,7 +111,6 @@ if (Meteor.isClient) {
 
         'click .js-set-image-filter': function(event) {
             Session.set("userFilter", this.createdBy);
-            Session.set("", value);
         }, // end js-set-image-filter
 
         'click .js-rm-image-filter': function(event) {
