01b44fea591d730c8e1e79bf7042dd2a70f4cd4b
Michael Check <mcheck@sologroup.com>
2016-02-02 22:45:02 -0600

    Change the layout into a grid

--- week-04/peer-assessment/siteace/client/siteace.js
+++ week-04/peer-assessment/siteace/client/siteace.js
@@ -1,35 +1,3 @@
-/////
-// routing
-/////
-
-Router.configure({
-  layoutTemplate: 'ApplicationLayout'
-});
-
-Router.route('/', function() {
-    this.render('navbar', {
-        to: "navbar"
-    });
-    this.render('website_list', {
-        to: "content"
-    });
-});
-
-Router.route('/website/:_id', function() {
-    this.render('navbar', {
-        to: "navbar"
-    });
-    this.render('website_detail', {
-        to: "content",
-        data: function(){
-          return Websites.findOne({
-            _id: this.params._id,
-          });
-        }
-    });
-});
-
-
 
 /////
 // user authentication
